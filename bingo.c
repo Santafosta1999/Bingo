81b5762d21c5a16f84145d4a08f43bc4a619d76b
Luiz Eduardo Santafosta <46579850+Santafosta1999@users.noreply.github.com>
2022-05-02 18:17:11 -0300

    Update bingo.c
    Código comentado

--- bingo.c
+++ bingo.c
@@ -4,33 +4,38 @@
 #include <Windows.h>
 
 typedef struct card {
-    /* Id do jogador, lista com 9 números aleatórios não repetidos */
+    /* Id do jogador e lista com 9 números aleatórios não repetidos */
     int idPlayer;
     int numbers[9];
 } CARD;
 
 typedef struct ranking {
+    /* Matriz, onde a primeira posição é o Id do jogador e 
+    a segunda a quantidade de números restantes na cartela */
     int classification[30][2];
 } RANKING;
 
 void randomNumbers();
 int printArray();
 
-int countDraw = 0;
-int drawNumbers[100];
-int countPlayers = 0;
-CARD listPlayers[30];
-RANKING ranking;
+int countDraw = 0; // Contador de números já sorteados
+int drawNumbers[100]; // Lista com os números já sorteados para não ter repetições
+int countPlayers = 0; // Contador de jogadores da partida
+CARD listPlayers[30]; // Lista de cartelas dos Jogadores
+RANKING ranking; // Ranking da partida
 
 int randomNewPlayer() {
+    // Verificar aleatóriamente se será adicionado um novo jogador ou não
     return rand() & 1;
 }
 
 int randomStartPlayer() {
+    // Definir aleatóriamente quantos jogadores iniciarão a partida
     return rand()%30;
 }
 
 int randomNum() {
+    // Retornar um número aleatório de 1 à 100
     int num = 0;
     while(num == 0) {
         num = rand()%100;
@@ -39,15 +44,19 @@ int randomNum() {
 }
 
 CARD *buildCard(int playerId) {
+    /* Criação de nova cartela, definindo o idPlayer e chamando um método
+    que irá preencher a lista com números aleatórios e não repetidos de 1 à 100 */
     CARD *c;
     c = malloc(sizeof(CARD));
     c->idPlayer = playerId;
-    randomNumbers(c->numbers, playerId);
+    randomNumbers(c->numbers);
 
     return c;
 }
 
-void randomNumbers(int array[], int player) {
+void randomNumbers(int array[]) {
+    /* Preencher a cartela com números aleatórios e não repetidos.
+    O segundo "while" verifica se o número já foi existe na cartela, se sim, sorteia novamente */
     int i = 0, num;
     while(i < 9) {
         int j = 0;
@@ -66,18 +75,24 @@ void randomNumbers(int array[], int player) {
     }
 }
 
-int printArray(int array[], int player) {
-    int i = 0;
-    printf("\n\nPLAYER %i\n\n", player);
-    while (i < 9)
-    {
-        printf("Pos %i - %i\n", i, (int)array[i]);
+void printCard(int idPlayer) {
+    // Imprime a cartela do jogador informado
+    int i = 0, j = 0;
+    printf("\n\nPLAYER %i\n\n", idPlayer);
+    while (i < countPlayers) {
+        if(listPlayers[i].idPlayer == idPlayer) {
+            while (j < 9) {
+                printf("Num %i - %i\n", j + 1, (int)listPlayers[i].numbers[j]);
+                j++;
+            }
+            break;
+        }
         i++;
     }
-    return 0;
 }
 
 RANKING *rankingHits() {
+    // Ordena o ranking da partida utilizando Selection Sort
     int i, j, min, id, num;
     for (i = 0; i < (countPlayers - 1); i++) {
         min = i;
@@ -100,12 +115,13 @@ RANKING *rankingHits() {
 }
 
 void writeRanking(RANKING *ranking) {
+    // Imprime na tela o Ranking e se houver ganhador, informa seu Id e imprime sua cartela
     int i = 0;
     printf("*-----------------------------*\n");
     printf("| POSICAO | JOGADOR | ACERTOS |\n");
     printf("|-----------------------------|\n");
     while(i < countPlayers) {
-        printf("|    %2i   |    %2i   |    %2i   |\n", i + 1, ranking->classification[i][0], 9 - (int)ranking->classification[i][1]);
+        printf("|    %2i   |    %2i   |    %i    |\n", i + 1, ranking->classification[i][0], 9 - (int)ranking->classification[i][1]);
         i++;
     }
     printf("*-----------------------------*\n\n\n");
@@ -116,6 +132,7 @@ void writeRanking(RANKING *ranking) {
         while(i < countPlayers) {
             if((int)ranking->classification[i][1] == 0) {
                 printf("JOGADOR %i!\n", (int)ranking->classification[i][0]);
+                printCard((int)ranking->classification[i][0]);
             }
             i++;
         }
@@ -124,6 +141,8 @@ void writeRanking(RANKING *ranking) {
 }
 
 int winner(int lastDraw) {
+    /* Verifica quem acertou na rodada, imprime o número da rodada,
+    o número sorteado e imprime o ranking, sempre verificando se há ganhador */
     int ret = 0, i = 0;   
     while (i < countPlayers) {
         int j = 0;
@@ -147,7 +166,7 @@ int winner(int lastDraw) {
 }
 
 int drawNumber() {
-    //Sorteia
+    // Sorteia o número da rodada, verificando se ele já não foi sorteado antes e se não há ganhador na partida
     int i = 0;
     int num = randomNum();
     if (countDraw != 0 && countDraw < 100) {
@@ -169,6 +188,7 @@ int drawNumber() {
 }
 
 void addPlayer(CARD *c) {
+    // Método que adiciona novo jogador, criando sua cartela, e adicionando na lista de jogadores e no Ranking
     countPlayers++;
     c = buildCard(countPlayers);
     listPlayers[countPlayers - 1] = *c;
@@ -180,7 +200,7 @@ int main() {
     CARD *c;
     c = malloc(sizeof(CARD));
 
-    srand(time(NULL));
+    srand(time(NULL)); // Utilizado para deixar o randon realmente aleatório
     
     int newDraw = 0, beginPlayers = randomStartPlayer();
     while (countPlayers <= beginPlayers) {
